9f1bb46234abd9e993993e5b4e555a06fe7e0f25
fujimorichihiro <fujimori.drum@gmail.com>
2021-02-18 00:24:05 +0900

    First Commit

--- swiftUiList/ContentView.swift
+++ swiftUiList/ContentView.swift
@@ -7,10 +7,54 @@
 
 import SwiftUI
 
+struct Player: Identifiable {
+    var id: Int
+    let name: String
+    let image: Image
+}
+
 struct ContentView: View {
+    
+    let players: [[Player]] = [
+        [
+            .init(id: 0, name: "James", image: Image("james")),
+            .init(id: 1, name: "Stephen", image: Image("stephen")),
+            .init(id: 2, name: "Lebron", image: Image("lebron"))
+        ],
+        
+        [.init(id: 0, name: "James", image: Image("james")),
+        .init(id: 1, name: "Stephen", image: Image("stephen")),
+        .init(id: 2, name: "Lebron", image: Image("lebron"))
+        ],
+        
+        [.init(id: 0, name: "James", image: Image("james")),
+        .init(id: 1, name: "Stephen", image: Image("stephen")),
+        .init(id: 2, name: "Lebron", image: Image("lebron"))
+        ]
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(players.startIndex...(players.endIndex - 1), id: \.self) {
+                    section in
+                    Section(header: Text("1990s")) {
+                        ForEach(self.players[section]) { player in
+                            HStack {
+                                player.image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .clipShape(Circle())
+                                    .frame(width: 50, height: 50)
+                                Text(player.name)
+                                    .foregroundColor(.black)
+                                    .padding(.leading, 20)
+                            }
+                        }
+                    }
+                }
+            }.navigationBarTitle(Text("NBA Player"))
+        }
     }
 }
 
