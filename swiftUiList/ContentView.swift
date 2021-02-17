941a7ef4062a3d79eea3b6b278737c84dbaeb699
fujimorichihiro <fujimori.drum@gmail.com>
2021-02-17 23:17:59 +0900

    Initial Commit

---
+++ swiftUiList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUiList
+//
+//  Created by 藤森千裕 on 2021/02/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUiList/swiftUiListApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUiListApp.swift
+//  swiftUiList
+//
+//  Created by 藤森千裕 on 2021/02/17.
+//
+
+import SwiftUI
+
+@main
+struct swiftUiListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
